ae37c54da2d3982d2aa0a569217348f5ac2f7967
Sahil Tembhare <isr.sahil.mk@gmail.com>
2021-09-10 12:55:39 +0530

    lib.rs has NFT struct and implememnted methods for it, bin/main is using lib for continuously getting random elements from the vec

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,6 +4,6 @@ fn main() {
 	let mut some = NFT::new(
 		vec!["one".to_string(), "two".to_string(), "three".to_string(), "four".to_string(), "five".to_string(), "six".to_string(), "seven".to_string(), "eight".to_string()]
 		);
-	println!("{:?}", some.get_next_bundle(4));
+	println!("{:?}", some.get_next_bundle(4)); 
 	println!("{:?}", some.get_next_bundle(4));
 }
